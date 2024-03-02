7e1834fff647ec320d4895f2ddcc23690c869031
Casey Ydenberg <hi@caydenberg.io>
2024-03-01 17:39:04 -0800

    Fix prism highlighting of jsx and tsx

--- server/content/Prism.ts
+++ server/content/Prism.ts
@@ -1,12 +1,12 @@
 import Prism from "https://esm.sh/prismjs@1.29.0";
 import typescript from "https://esm.sh/v131/prismjs@1.29.0/denonext/components/prism-typescript.js";
-import jsx from "https://esm.sh/v131/prismjs@1.29.0/denonext/components/prism-jsx.js";
+import jsx from "https://esm.sh/v131/prismjs@1.29.0/es2022/components/prism-jsx.min.js";
 import tsx from "https://esm.sh/v131/prismjs@1.29.0/denonext/components/prism-tsx.js";
 import type { Lang } from "./MdastNode.ts";
 
 Prism.languages.typescript = typescript;
-Prism.languages.jsx = jsx;
-Prism.languages.tsx = tsx;
+Prism.languages.PrismJsx = jsx;
+Prism.languages.PrismTsx = tsx;
 
 export default (code: string | undefined, lang: Lang | undefined) => {
   if (!code) return "";
